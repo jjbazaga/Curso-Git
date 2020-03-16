a97de7e80eb78c22dae3f0c7e10890d5533ab00c
JJBAZAGA <jjbazaga@gmail.com>
2020-03-16 17:02:53 -0300

    Projeto Inicial - Alo Word!

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Estudando_Git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
