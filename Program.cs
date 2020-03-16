10a2f5654e8cafd4b7fcb711225c5b377bcd1ff9
JJBAZAGA <jjbazaga@gmail.com>
2020-03-16 17:08:50 -0300

    Acrescentado Ola tudo bem?

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace Estudando_Git
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            System.Console.WriteLine("Acrescentando Ola tudo bem?");
         }
     }
 }
